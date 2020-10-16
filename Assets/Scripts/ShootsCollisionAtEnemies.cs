5b9357522f637583d4cf80499be028d11b70f7ab
Douglas Dias <douglas.dias2102@outlook.com>
2020-10-16 00:44:19 -0300

    Adds one enemy ship, new materials, particle effects and particle collisions The player is now able to destroy enemies

---
+++ Assets/Scripts/ShootsCollisionAtEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootsCollisionAtEnemies : MonoBehaviour
+{
+    public ParticleSystem part;
+    public ParticleCollisionEvent[] collisionEvents;
+    public GameObject explosionPrefab;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new ParticleCollisionEvent[60];
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Collide");
+        GameObject exp = Instantiate(explosionPrefab, other.transform.position, other.transform.rotation);
+
+        foreach (Transform child in other.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        Destroy(other);
+    }
+}
